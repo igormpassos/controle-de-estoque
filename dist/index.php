89e30dcaddb447362301a0205ca6fa648d4eef74
Igor Marques <igormpassos12@gmail.com>
2024-09-26 22:51:59 -0300

    Update project files

--- dist/index.php
+++ dist/index.php
@@ -34,63 +34,64 @@ $totalManutencoes = $resultTotalManutencoes->first()->get('totalManutencoes');
 
 <div class="main-panel">
     <div class="content-wrapper">
-        <div class="row">
-            <!-- Cards com cores personalizadas -->
-            <div class="col-md-3 grid-margin stretch-card">
-                <a href="/controle-de-estoque/dist/pages/equipamento/index.php">
-                    <div class="card bg-primary text-white">
-                        <div class="card-body">
-                            <h4 class="card-title">Total de Equipamentos</h4>
-                            <p class="card-text"><?php echo $totalEquipamentos; ?></p>
-                        </div>
-                    </div>
-                </a>
+    <div class="row d-flex justify-content-between flex-wrap">
+    <!-- Cards com cores personalizadas -->
+    <div class="col-md-2 grid-margin">
+        <a href="/controle-de-estoque/dist/pages/equipamento/index.php">
+            <div class="card bg-primary text-white">
+                <div class="card-body">
+                    <h4 class="card-title">Total de Equipamentos</h4>
+                    <p class="card-text"><?php echo $totalEquipamentos; ?></p>
+                </div>
             </div>
+        </a>
+    </div>
 
-            <div class="col-md-3 grid-margin stretch-card">
-                <a href="/controle-de-estoque/dist/pages/equipamento/index.php?status=disponivel">
-                    <div class="card bg-success text-white">
-                        <div class="card-body">
-                            <h4 class="card-title">Equipamentos Disponíveis</h4>
-                            <p class="card-text"><?php echo $totalDisponiveis; ?></p>
-                        </div>
-                    </div>
-                </a>
+    <div class="col-md-2 grid-margin">
+        <a href="/controle-de-estoque/dist/pages/equipamento/index.php?status=disponivel">
+            <div class="card bg-success text-white">
+                <div class="card-body">
+                    <h4 class="card-title">Equipamentos Disponíveis</h4>
+                    <p class="card-text"><?php echo $totalDisponiveis; ?></p>
+                </div>
             </div>
+        </a>
+    </div>
 
-            <div class="col-md-3 grid-margin stretch-card">
-                <a href="/controle-de-estoque/dist/pages/equipamento/index.php?status=em_uso">
-                    <div class="card bg-warning text-white">
-                        <div class="card-body">
-                            <h4 class="card-title">Equipamentos em Uso</h4>
-                            <p class="card-text"><?php echo $totalEmUso; ?></p>
-                        </div>
-                    </div>
-                </a>
+    <div class="col-md-2 grid-margin">
+        <a href="/controle-de-estoque/dist/pages/equipamento/index.php?status=em_uso">
+            <div class="card bg-warning text-white">
+                <div class="card-body">
+                    <h4 class="card-title">Equipamentos em Uso</h4>
+                    <p class="card-text"><?php echo $totalEmUso; ?></p>
+                </div>
             </div>
+        </a>
+    </div>
 
-            <div class="col-md-3 grid-margin stretch-card">
-                <a href="/controle-de-estoque/dist/pages/equipamento/index.php?status=manutencao">
-                    <div class="card bg-danger text-white">
-                        <div class="card-body">
-                            <h4 class="card-title">Equipamentos em Manutenção</h4>
-                            <p class="card-text"><?php echo $totalManutencao; ?></p>
-                        </div>
-                    </div>
-                </a>
+    <div class="col-md-2 grid-margin">
+        <a href="/controle-de-estoque/dist/pages/equipamento/index.php?status=manutencao">
+            <div class="card bg-danger text-white">
+                <div class="card-body">
+                    <h4 class="card-title">Equipamentos em Manutenção</h4>
+                    <p class="card-text"><?php echo $totalManutencao; ?></p>
+                </div>
             </div>
+        </a>
+    </div>
 
-            <div class="col-md-3 grid-margin stretch-card">
-                <a href="/controle-de-estoque/dist/pages/manutencao/index.php">
-                    <div class="card bg-info text-white">
-                        <div class="card-body">
-                            <h4 class="card-title">Total de Manutenções</h4>
-                            <p class="card-text"><?php echo $totalManutencoes; ?></p>
-                        </div>
-                    </div>
-                </a>
+    <div class="col-md-2 grid-margin">
+        <a href="/controle-de-estoque/dist/pages/manutencao/index.php">
+            <div class="card bg-info text-white">
+                <div class="card-body">
+                    <h4 class="card-title">Total de Manutenções</h4>
+                    <p class="card-text"><?php echo $totalManutencoes; ?></p>
+                </div>
             </div>
-        </div>
+        </a>
+    </div>
+</div>
+
 
         <!-- Tabelas com Equipamentos Disponíveis e Em Uso -->
         <div class="row">
